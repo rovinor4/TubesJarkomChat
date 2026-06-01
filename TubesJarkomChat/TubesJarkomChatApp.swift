0aed0ab3f6ee2736b8ccbf317402e4d74456ce1e
Rovino Ramadhani <70685780+rovinor4@users.noreply.github.com>
2026-06-01 22:51:08 +0700

    Initial Commit

---
+++ TubesJarkomChat/TubesJarkomChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubesJarkomChatApp.swift
+//  TubesJarkomChat
+//
+//  Created by Rovino Ramadhani on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TubesJarkomChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
